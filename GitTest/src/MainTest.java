b2c58458bd8b68c1c974be185c62bea0183f08fe
poop-code <x2d16x5@gmail.com>
2016-09-02 09:29:59 +0900

    강제

--- GitTest/src/MainTest.java
+++ GitTest/src/MainTest.java
@@ -6,8 +6,7 @@ public class MainTest {
 		
 		System.out.println("테스트");
 		System.out.println("테스트2");
-		System.out.println("테스트3");
-		System.out.println("테스트4");
+		System.out.println("테스트ㅁㅁㅁ");
 	}
 
 }
